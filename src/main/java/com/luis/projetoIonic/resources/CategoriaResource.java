3c6edea9a44778655ba528f7e9d65b72fc2e80c9
André luis <luis.alms91@gmail.com>
2018-09-19 18:39:11 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/luis/projetoIonic/resources/CategoriaResource.java
+++ src/main/java/com/luis/projetoIonic/resources/CategoriaResource.java
@@ -1,15 +1,31 @@
 package com.luis.projetoIonic.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luis.projetoIonic.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Funcioando";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria();
+		cat.setId(1);
+		cat.setNome("Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		//Criação do array liste pois o liste como
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
